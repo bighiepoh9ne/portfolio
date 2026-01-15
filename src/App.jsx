caf294050f45e3446e479ace27805baadf6de1d5
Akik <153067310+bighiepoh9ne@users.noreply.github.com>
2026-01-16 00:15:46 +0100

    feat: implement routing with react-router-dom and set up main application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
-npm install react-router-dom
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
+import Parcours from "./pages/Parcours"
+import Experience from "./pages/Experience"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/parcours" element={<Parcours />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
