98f1e8d4e87d9d2854529be1cf2435b773099296
Akik <153067310+bighiepoh9ne@users.noreply.github.com>
2026-07-10 19:22:47 +0200

    fix: set basename for BrowserRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Contact from "./pages/Contact"
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <StarryBackground />
       <Navbar />
 
